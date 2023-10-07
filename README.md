b8eedee373ed0bbd8d38d20fcae86ada616d7c33
gaimo-ch <gaimovski.dev@gmail.com>
2023-10-07 17:02:41 +0000

    :art: Update Design

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+<h1>
+<!-- <a href="https://gaimo.ch"> -->
+    ｇａｉｍｏ．ｃｈ
+  </a>
+</h1>
+<p>
+  <b>
+    🌴⛲️🐬　Ａｅｓｔｈｅｔｉｃ　Ｗｅｂ　Ｄｅｓｉｇｎ　🐬⛲️🌴<br>
+    ＨＴＭＬ　／　ＳＣＳＳ　／　ＪａｖａＳｃｒｉｐｔ<br>
+    🐬Ｃｏｍｉｎｇ　ｓｏｏｎ...🐬<br>
+  </b>
+</p>
+<img src="public/img/README.png">
+</div>
--- public/zenn.js
+++ public/zenn.js
@@ -1,34 +1,58 @@
+function displayArticles(articles) {
+    const articleListContainer = document.querySelector('.articles-main');
+    const articleList = document.createElement('ul');
+
+    articles.forEach(article => {
+        const listItem = createArticleListItem(article);
+        articleList.appendChild(listItem);
+    });
+
+    articleListContainer.appendChild(articleList);
+}
+
+function createArticleListItem(article) {
+    const link = document.createElement('a');
+    link.href = `https://zenn.dev${article.path}`;
+    link.target = "_blank";
+
+    const dateContainer = document.createElement('div');
+    dateContainer.classList.add('date-container');
+
+    const zennImage = document.createElement('img');
+    zennImage.src = 'img/zenn.svg';
+    zennImage.classList.add('zenn');
+
+    const publishedAt = document.createElement('p');
+    publishedAt.textContent = formatDate(article.published_at);
+
+    const title = document.createElement('p');
+    title.textContent = article.title;
+
+    dateContainer.appendChild(zennImage);
+    dateContainer.appendChild(publishedAt);
+
+    link.appendChild(dateContainer);
+    link.appendChild(title);
+
+    const listItem = document.createElement('a');
+    listItem.appendChild(link);
+
+    return listItem;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch('/api/articles')
         .then(response => response.json())
         .then(data => {
-            const articleListContainer = document.querySelector('.articles-main');
-
-            const articles = data.articles;
-            const articleList = document.createElement('ul');
-
-            articles.forEach(article => {
-                const listItem = document.createElement('li');
-                const publishedAt = document.createElement('p');
-                const path = document.createElement('a');
-
-                const date = new Date(article.published_at);
-                const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
-
-                publishedAt.textContent = `${formattedDate}`;
-                path.textContent = article.title;
-                path.href = `https://zenn.dev${article.path}`;
-                path.target = "_blank";
-
-                listItem.appendChild(publishedAt);
-                listItem.appendChild(path);
-
-                articleList.appendChild(listItem);
-            });
-
-            articleListContainer.appendChild(articleList);
+            displayArticles(data.articles);
         })
         .catch(error => {
             console.error('エラー:', error);
         });
 });
+
+function formatDate(dateString) {
+    const date = new Date(dateString);
+    const formattedDate = `${date.getFullYear()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getDate().toString().padStart(2, '0')}`;
+    return formattedDate;
+}
